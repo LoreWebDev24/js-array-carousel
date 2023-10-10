564e5cc3ac1a95c963dc5377c94926c3e8fef168
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-10 15:43:24 +0200

    add js  that adds images in the html from array

---
+++ js/master.js
@@ -0,0 +1,25 @@
+
+let srcImagesArray = ["./img/01.webp","./img/02.webp","./img/03.webp","./img/04.webp","./img/05.webp"]
+
+console.log(srcImagesArray)
+
+let containerDOMElement = document.querySelector('.container-big')
+
+console.log(containerDOMElement)
+
+
+for (let i = 0; i < srcImagesArray.length; i++ ) {
+    let currentSrc = srcImagesArray[i] ;
+    console.log(currentSrc)
+
+    let htmlString = `
+    <div class="image"> 
+        <img src="${currentSrc}">
+    </div>
+    `
+
+    containerDOMElement.innerHTML += htmlString
+}
+
+let imageDOMElement = document.querySelectorAll('.image')
+console.log(imageDOMElement)
